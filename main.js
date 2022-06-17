4dd7e331fea4e5e0308d2802cdba1b97798c4aa5
Radwan AlShawesh <radwan.al.shawesh@hotmail.com>
2022-06-17 18:09:53 +0800

    form

--- main.js
+++ main.js
@@ -10,14 +10,24 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
 
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-
-        console.log(title, author, pages)
 
 
 }
 
-const addBook = document.querySelector('#addBook');
-addBook.addEventListener('click', addBookToLibrary);
+function validate() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+
+    console.log(read)
+
+}
+
+
+
+
+
+
+const addBook = document.getElementById('add_book');
+addBook.addEventListener('click', validate);
