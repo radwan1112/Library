00cd5da361ac6d805024eaed944100341b519ce3
Radwan AlShawesh <radwan.al.shawesh@hotmail.com>
2022-06-17 16:20:01 +0800

    validation

--- main.js
+++ main.js
@@ -8,12 +8,16 @@ function Book(title, author, pages, read) {
 }
 
 
-const book = new Book('harry potter', 'nobody', 300, true);
-
-console.log(book);
-
 function addBookToLibrary() {
 
-    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+
+        console.log(title, author, pages)
+
 
 }
+
+const addBook = document.querySelector('#addBook');
+addBook.addEventListener('click', addBookToLibrary);
