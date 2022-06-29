67fcd1b900319460a8653ef640334a891e8d8193
Radwan AlShawesh <radwan.al.shawesh@hotmail.com>
2022-06-29 19:33:50 +0800

    new

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-
-function addBookToLibrary() {
-
-
-
-}
+function addBookToLibrary() {}
 
 function validate() {
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').checked;
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const read = document.getElementById('read').checked
 
-    console.log(read)
-
+  console.log(read)
 }
 
+function doForm() {
+  let data = new FormData()
 
+  data.append('book', document.getElementById('book_title').value)
+  data.append('author', document.getElementById('book_author').value)
+  data.append('pages', document.getElementById('pages').value)
+  data.append('read', document.getElementById('read').checked)
 
-
-
-
-const addBook = document.getElementById('add_book');
-addBook.addEventListener('click', validate);
+  for (let [t, a, p, r] of data.entries()) {
+    console.log(t, a, p, r)
+  }
+  return false
+}
