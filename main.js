f864d6da58696dd95fe311e39d55ba2b298d66a2
Radwan AlShawesh <radwan.al.shawesh@hotmail.com>
2022-07-01 15:22:21 +0800

    asdasd

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ function validate() {
   const author = document.getElementById('author').value
   const pages = document.getElementById('pages').value
   const read = document.getElementById('read').checked
-
-  console.log(read)
 }
 
 function doForm() {
@@ -31,3 +29,23 @@ function doForm() {
   }
   return false
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
